fb3cdef1cb154d254d546df476b61319a74f8ae3
sachingangwar-i <sachin.gangwar@indmoney.com>
2025-04-03 01:14:24 +0530

    added recursive insertion sort

---
+++ striver_AtoZ/sorting/rec_insertion_sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void rIsort(vector<int> &v, int i, int n) {
+    if(i==n) {
+        return;
+    }
+    int j = i-1;  
+    while(j>=0 && v[j]>v[j+1]) {
+        swap(v[j+1], v[j]);
+        j--;
+    }
+    rIsort(v, i+1, n);
+}
+
+int main() {
+    int arr[] = {4, 5, 1, 2, 8, 3, 7, 9, 6};
+    vector<int> v(arr, arr+9);
+    int n = v.size();
+    rIsort(v, 1, n);   
+    cout<<"Sorted array: ";
+    for(int i=0; i<n; i++) {
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
